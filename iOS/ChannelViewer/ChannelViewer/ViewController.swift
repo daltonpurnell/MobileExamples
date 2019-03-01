31566b5d2462856830500986cbd4ac8cb9838e47
Yevgen Marchuk <yevgen.marchuk@phenixrts.com>
2019-03-02 01:14:06 +0200

    [iOS Application] Implement simple subscribe app (app structure) (#DEMO-541) In Progress)
    Change-Id: I622567fd0e0c48ff7996fa118dc293c622925714

---
+++ iOS/ChannelViewer/ChannelViewer/ViewController.swift
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2019 Phenix Real Time Solutions, Inc. Confidential and Proprietary. All Rights Reserved.
+ *
+ * By using this code you agree to the Phenix Terms of Service found online here:
+ * http://phenixrts.com/terms-of-service.html
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
